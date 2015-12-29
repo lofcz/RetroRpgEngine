41d0373000415a9677c1ae4acc19817cfe0265f6
kocourmatej <kocourmatej@centrum.cz>
2015-12-29 22:10:44 +0100

    Level editor

---
+++ RetroRpgLevelEditor/RetroRpgLevelEditor/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RetroRpgLevelEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.oSave = new System.Windows.Forms.Button();
+            this.oLoad = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // oSave
+            // 
+            this.oSave.Location = new System.Drawing.Point(799, 12);
+            this.oSave.Name = "oSave";
+            this.oSave.Size = new System.Drawing.Size(174, 74);
+            this.oSave.TabIndex = 0;
+            this.oSave.Text = "Save";
+            this.oSave.UseVisualStyleBackColor = true;
+            this.oSave.Click += new System.EventHandler(this.oSave_Click);
+            // 
+            // oLoad
+            // 
+            this.oLoad.Location = new System.Drawing.Point(798, 112);
+            this.oLoad.Name = "oLoad";
+            this.oLoad.Size = new System.Drawing.Size(174, 77);
+            this.oLoad.TabIndex = 1;
+            this.oLoad.Text = "Load";
+            this.oLoad.UseVisualStyleBackColor = true;
+            this.oLoad.Click += new System.EventHandler(this.oLoad_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(805, 223);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(166, 28);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 518);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.oLoad);
+            this.Controls.Add(this.oSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button oSave;
+        private System.Windows.Forms.Button oLoad;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ RetroRpgLevelEditor/RetroRpgLevelEditor/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UrielGuy.SyntaxHighlightingTextBox;
+using System.IO;
+
+namespace RetroRpgLevelEditor
+{
+    public partial class Form1 : Form
+    {
+        SyntaxHighlightingTextBox shtb;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            shtb = new SyntaxHighlightingTextBox();
+            shtb.Location = new Point(0, 0);
+            shtb.Dock = DockStyle.Left;
+            shtb.Width = 800;
+            shtb.Font = new Font("LucidaConsole", 12, FontStyle.Bold);
+           
+            shtb.Seperators.Add(' ');
+            shtb.Seperators.Add('\r');
+            shtb.Seperators.Add('\n');
+            shtb.Seperators.Add(',');
+            shtb.Seperators.Add('.');
+            shtb.Seperators.Add('-');
+            shtb.Seperators.Add('+');
+            //shtb.Seperators.Add('*');
+            //shtb.Seperators.Add('/');
+            Controls.Add(shtb);
+            shtb.WordWrap = false;
+            shtb.ScrollBars = RichTextBoxScrollBars.Both;// & RichTextBoxScrollBars.ForcedVertical;
+
+            shtb.FilterAutoComplete = false;
+            /*shtb.HighlightDescriptors.Add(new HighlightDescriptor("<", Color.Gray, null, DescriptorType.Word, DescriptorRecognition.WholeWord, true));
+
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("<<", ">>", Color.DarkGreen, null, DescriptorType.ToCloseToken, DescriptorRecognition.StartsWith, false));
+*/
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("P", Color.Blue, null, DescriptorType.Word, DescriptorRecognition.WholeWord, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("E", Color.Red, null, DescriptorType.Word, DescriptorRecognition.WholeWord, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("#", Color.DarkSlateGray, null, DescriptorType.Word, DescriptorRecognition.Contains, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("World", Color.Blue, null, DescriptorType.Word, DescriptorRecognition.WholeWord, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("//", Color.Green, null, DescriptorType.ToEOL, DescriptorRecognition.StartsWith, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("[", "]", Color.Red, null, DescriptorType.ToCloseToken, DescriptorRecognition.WholeWord, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("@DataPipeline", Color.Orange, null, DescriptorType.Word, DescriptorRecognition.WholeWord, true));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("@DesignPipeline", Color.Orange, null, DescriptorType.Word, DescriptorRecognition.WholeWord, true));
+           
+        }
+
+
+
+        private void oSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter save = new StreamWriter("map.RetroRpgMap");
+
+            bool foundDataLine = false;
+            int dataLine = 0;
+            foreach (string line in shtb.Lines)
+            {
+                dataLine++;
+
+                if (line == "#DataPipe")
+                {
+                    break;
+                }
+            }
+
+
+            foreach (string line in shtb.Lines)
+            {
+                if (line.StartsWith("//"))
+                {
+                    save.WriteLine(line);
+                }
+                else if (line.StartsWith("#DataLine"))
+                {
+                    save.WriteLine(line);
+                }
+                else
+                {
+                    foreach (char znak in line)
+                    {
+                        if (znak == '#')
+                        {
+                            save.Write("[oWall]");
+                        }
+                        else if (znak == ' ')
+                        {
+                            save.Write("[blank]");
+                        }
+                        else if (znak == '¤')
+                        {
+                            
+                                save.Write("[blank]");
+                        }
+                        save.WriteLine();
+
+                    }
+                    save.WriteLine("#NewLine");
+                }
+            }
+
+            save.Close();
+        }
+
+        private void oLoad_Click(object sender, EventArgs e)
+        {
+        //    backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+        //    backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(bgw_ProgressChanged);
+         //   backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+           // backgroundWorker1.WorkerReportsProgress = true;
+          //  backgroundWorker1.RunWorkerAsync();
+
+
+            StreamReader load = new StreamReader("map.RetroRpgMap");
+            string line = "";
+            int total_lines = File.ReadLines("map.RetroRpgMap").Count();
+            int current_line = 0;
+
+            while ((line = load.ReadLine()) != null)
+            {
+                current_line++;
+                if (line.StartsWith("//"))
+                {
+                    SetText(line);//  shtb.Text += line;
+                    SetText("\n");  //shtb.Text += "\n";
+                }
+                else if (line.Contains("[oWall]"))
+                {
+                    //shtb.Text += "#";
+                    SetText("#");
+                }
+                else if (line.Contains("[blank]"))
+                {
+                    // shtb.Text += " ";
+                    SetText(" ");
+                }
+                else if (line == "#NewLine")
+                {
+                    //shtb.Text += "\n";
+                    SetText("\n");
+                }
+
+               // int percents = ((current_line * 100) / total_lines);
+              //  backgroundWorker1.ReportProgress(percents, 1);
+            }
+            load.Close();
+        }
+
+        void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;          
+        }
+
+        void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //do the code when bgv completes its work
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StreamReader load = new StreamReader("map.RetroRpgMap");
+            string line = "";
+            int total_lines = File.ReadLines("map.RetroRpgMap").Count();
+            int current_line = 0;
+
+            while ((line = load.ReadLine()) != null)
+            {
+                current_line++;
+                if (line.StartsWith("//"))
+                {
+                    SetText(line);//  shtb.Text += line;
+                    SetText("\n");  //shtb.Text += "\n";
+                }
+                else if (line.Contains("[oWall]"))
+                {
+                    //shtb.Text += "#";
+                    SetText("#");
+                }
+                else if (line.Contains("[blank]"))
+                {
+                   // shtb.Text += " ";
+                    SetText(" ");
+                }
+                else if (line == "#NewLine")
+                {
+                    //shtb.Text += "\n";
+                    SetText("\n");
+                }
+
+                int percents = ((current_line * 100) / total_lines) ;
+                backgroundWorker1.ReportProgress(percents, 1);
+            }
+            load.Close();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.shtb.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.shtb.Text += text;
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
