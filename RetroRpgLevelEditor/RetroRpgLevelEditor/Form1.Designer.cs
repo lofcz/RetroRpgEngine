a1abc51ac97399a7010e6924ea121ef98f85191e
kocourmatej <kocourmatej@centrum.cz>
2015-12-30 18:03:14 +0100

    Update level editoru
    Level editor nyní umí kompilovat, automaticky přiřadit ID objektu,
    exportovoat vlastnosti objektů.

--- RetroRpgLevelEditor/RetroRpgLevelEditor/Form1.Designer.cs
+++ RetroRpgLevelEditor/RetroRpgLevelEditor/Form1.Designer.cs
@@ -34,13 +34,19 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.oCompile = new System.Windows.Forms.Button();
+            this.oIDEditor = new System.Windows.Forms.RichTextBox();
+            this.oZoomPlus = new System.Windows.Forms.Button();
+            this.oZoomMinus = new System.Windows.Forms.Button();
+            this.oZoomCenter = new System.Windows.Forms.Button();
+            this.CompileCode = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // oSave
             // 
-            this.oSave.Location = new System.Drawing.Point(799, 12);
+            this.oSave.Location = new System.Drawing.Point(873, 3);
             this.oSave.Name = "oSave";
-            this.oSave.Size = new System.Drawing.Size(174, 74);
+            this.oSave.Size = new System.Drawing.Size(174, 50);
             this.oSave.TabIndex = 0;
             this.oSave.Text = "Save";
             this.oSave.UseVisualStyleBackColor = true;
@@ -48,9 +54,9 @@
             // 
             // oLoad
             // 
-            this.oLoad.Location = new System.Drawing.Point(798, 112);
+            this.oLoad.Location = new System.Drawing.Point(873, 59);
             this.oLoad.Name = "oLoad";
-            this.oLoad.Size = new System.Drawing.Size(174, 77);
+            this.oLoad.Size = new System.Drawing.Size(174, 43);
             this.oLoad.TabIndex = 1;
             this.oLoad.Text = "Load";
             this.oLoad.UseVisualStyleBackColor = true;
@@ -62,21 +68,93 @@
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
             this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(805, 223);
+            this.progressBar1.Location = new System.Drawing.Point(873, 347);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(166, 28);
+            this.progressBar1.Size = new System.Drawing.Size(195, 28);
             this.progressBar1.TabIndex = 2;
+            // 
+            // oCompile
+            // 
+            this.oCompile.Location = new System.Drawing.Point(873, 108);
+            this.oCompile.Name = "oCompile";
+            this.oCompile.Size = new System.Drawing.Size(170, 43);
+            this.oCompile.TabIndex = 3;
+            this.oCompile.Text = "Add ID";
+            this.oCompile.UseVisualStyleBackColor = true;
+            this.oCompile.Click += new System.EventHandler(this.oCompile_Click);
+            // 
+            // oIDEditor
+            // 
+            this.oIDEditor.AcceptsTab = true;
+            this.oIDEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 3F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.oIDEditor.Location = new System.Drawing.Point(872, 381);
+            this.oIDEditor.Name = "oIDEditor";
+            this.oIDEditor.Size = new System.Drawing.Size(196, 164);
+            this.oIDEditor.TabIndex = 4;
+            this.oIDEditor.Text = "";
+            // 
+            // oZoomPlus
+            // 
+            this.oZoomPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.oZoomPlus.Location = new System.Drawing.Point(1000, 292);
+            this.oZoomPlus.Name = "oZoomPlus";
+            this.oZoomPlus.Size = new System.Drawing.Size(58, 49);
+            this.oZoomPlus.TabIndex = 5;
+            this.oZoomPlus.Text = "+";
+            this.oZoomPlus.UseVisualStyleBackColor = true;
+            this.oZoomPlus.Click += new System.EventHandler(this.oZoomPlus_Click);
+            // 
+            // oZoomMinus
+            // 
+            this.oZoomMinus.AllowDrop = true;
+            this.oZoomMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.oZoomMinus.Location = new System.Drawing.Point(936, 292);
+            this.oZoomMinus.Name = "oZoomMinus";
+            this.oZoomMinus.Size = new System.Drawing.Size(58, 49);
+            this.oZoomMinus.TabIndex = 6;
+            this.oZoomMinus.Text = "-";
+            this.oZoomMinus.UseVisualStyleBackColor = true;
+            this.oZoomMinus.Click += new System.EventHandler(this.oZoomMinus_Click);
+            // 
+            // oZoomCenter
+            // 
+            this.oZoomCenter.AllowDrop = true;
+            this.oZoomCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.oZoomCenter.Location = new System.Drawing.Point(872, 292);
+            this.oZoomCenter.Name = "oZoomCenter";
+            this.oZoomCenter.Size = new System.Drawing.Size(58, 49);
+            this.oZoomCenter.TabIndex = 7;
+            this.oZoomCenter.Text = "=";
+            this.oZoomCenter.UseVisualStyleBackColor = true;
+            this.oZoomCenter.Click += new System.EventHandler(this.oZoomCenter_Click);
+            // 
+            // CompileCode
+            // 
+            this.CompileCode.Location = new System.Drawing.Point(873, 162);
+            this.CompileCode.Name = "CompileCode";
+            this.CompileCode.Size = new System.Drawing.Size(169, 50);
+            this.CompileCode.TabIndex = 8;
+            this.CompileCode.Text = "Compile";
+            this.CompileCode.UseVisualStyleBackColor = true;
+            this.CompileCode.Click += new System.EventHandler(this.CompileCode_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 518);
+            this.ClientSize = new System.Drawing.Size(1070, 547);
+            this.Controls.Add(this.CompileCode);
+            this.Controls.Add(this.oZoomCenter);
+            this.Controls.Add(this.oZoomMinus);
+            this.Controls.Add(this.oZoomPlus);
+            this.Controls.Add(this.oIDEditor);
+            this.Controls.Add(this.oCompile);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.oLoad);
             this.Controls.Add(this.oSave);
@@ -94,6 +172,12 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button oCompile;
+        private System.Windows.Forms.RichTextBox oIDEditor;
+        private System.Windows.Forms.Button oZoomPlus;
+        private System.Windows.Forms.Button oZoomMinus;
+        private System.Windows.Forms.Button oZoomCenter;
+        private System.Windows.Forms.Button CompileCode;
     }
 }
 
--- RetroRpgLevelEditor/RetroRpgLevelEditor/Form1.cs
+++ RetroRpgLevelEditor/RetroRpgLevelEditor/Form1.cs
@@ -15,6 +15,7 @@ namespace RetroRpgLevelEditor
     public partial class Form1 : Form
     {
         SyntaxHighlightingTextBox shtb;
+        SyntaxHighlightingTextBox shtb2;
 
         public Form1()
         {
@@ -26,9 +27,9 @@ namespace RetroRpgLevelEditor
             shtb = new SyntaxHighlightingTextBox();
             shtb.Location = new Point(0, 0);
             shtb.Dock = DockStyle.Left;
-            shtb.Width = 800;
+            shtb.Width = 850;
             shtb.Font = new Font("LucidaConsole", 12, FontStyle.Bold);
-           
+           // shtb.DoubleClick += new EventHandler(shtbDoubleClick);
             shtb.Seperators.Add(' ');
             shtb.Seperators.Add('\r');
             shtb.Seperators.Add('\n');
@@ -49,7 +50,7 @@ namespace RetroRpgLevelEditor
 */
             shtb.HighlightDescriptors.Add(new HighlightDescriptor("P", Color.Blue, null, DescriptorType.Word, DescriptorRecognition.WholeWord, false));
             shtb.HighlightDescriptors.Add(new HighlightDescriptor("E", Color.Red, null, DescriptorType.Word, DescriptorRecognition.WholeWord, false));
-            shtb.HighlightDescriptors.Add(new HighlightDescriptor("#", Color.DarkSlateGray, null, DescriptorType.Word, DescriptorRecognition.Contains, false));
+            shtb.HighlightDescriptors.Add(new HighlightDescriptor("#", Color.Gray, null, DescriptorType.Word, DescriptorRecognition.Contains, false));
             shtb.HighlightDescriptors.Add(new HighlightDescriptor("World", Color.Blue, null, DescriptorType.Word, DescriptorRecognition.WholeWord, false));
             shtb.HighlightDescriptors.Add(new HighlightDescriptor("//", Color.Green, null, DescriptorType.ToEOL, DescriptorRecognition.StartsWith, false));
             shtb.HighlightDescriptors.Add(new HighlightDescriptor("[", "]", Color.Red, null, DescriptorType.ToCloseToken, DescriptorRecognition.WholeWord, false));
@@ -63,14 +64,18 @@ namespace RetroRpgLevelEditor
         private void oSave_Click(object sender, EventArgs e)
         {
             StreamWriter save = new StreamWriter("map.RetroRpgMap");
+            int x = 0;
+            int y = 0;
 
             bool foundDataLine = false;
             int dataLine = 0;
+
+
             foreach (string line in shtb.Lines)
             {
                 dataLine++;
 
-                if (line == "#DataPipe")
+                if (line == "@DataPipeline")
                 {
                     break;
                 }
@@ -79,36 +84,118 @@ namespace RetroRpgLevelEditor
 
             foreach (string line in shtb.Lines)
             {
-                if (line.StartsWith("//"))
-                {
-                    save.WriteLine(line);
-                }
-                else if (line.StartsWith("#DataLine"))
-                {
-                    save.WriteLine(line);
-                }
-                else
-                {
-                    foreach (char znak in line)
+              
+                    if (line.StartsWith("//"))
                     {
-                        if (znak == '#')
-                        {
-                            save.Write("[oWall]");
-                        }
-                        else if (znak == ' ')
-                        {
-                            save.Write("[blank]");
-                        }
-                        else if (znak == '¤')
-                        {
-                            
-                                save.Write("[blank]");
-                        }
-                        save.WriteLine();
-
+                        save.WriteLine(line);
                     }
-                    save.WriteLine("#NewLine");
-                }
+                    else if (line.StartsWith("@DataPipeline"))
+                    {
+                      //  save.WriteLine(line);
+                    }
+                    else
+                    {
+                        foreach (char znak in line)
+                        {
+                            if (znak == '#')
+                            {
+                                save.Write("[oWall]");
+                            }
+                            else if (znak == ' ')
+                            {
+                                save.Write("[blank]");
+                            }
+                            else if (znak == '¤')
+                            {
+
+                            bool parsing = false;
+                            int xx = 0;
+                            string myID = "";
+
+                            // ZJISTÍM SVOJE ID
+                            foreach (char znak1 in line)
+                            {
+                                xx++;
+
+                                if (znak1 == '[' && parsing == false && xx > x)
+                                {
+                                    parsing = true;
+                                }
+                                else if (parsing)
+                                {
+                                    if (znak1 != ']' && znak1 != ' ')
+                                    {
+                                        myID += znak1;
+                                    }
+                                    if (znak1 == ']')
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                               string csvParametrs = "";
+
+                                for (int i = dataLine; i < shtb.Lines.Count(); i++ )
+                                {
+                                    int parsingInformation = 0;
+                                    string yy = "";
+                                    string value = "";
+                                    string line1 = shtb.Lines[i];
+                                    string id = "";
+
+
+                                    // Naparsování pipeliny
+                                    foreach(char znak1 in line1)
+                                    {
+                                        if (znak1 == '[')
+                                        {
+                                            parsingInformation++;
+                                            if (parsingInformation == 2)
+                                            {
+                                                if (id != myID)
+                                                {
+                                                    break;
+                                                }
+                                                else
+                                                {
+                                                    //MessageBox.Show("Našel jsem shodu v ID");
+                                                }
+
+                                            }
+                                        }
+                                        else if (znak1 != ' ' && znak1 != ']')
+                                        {
+                                            if (parsingInformation == 1) // Parsuju ID
+                                            {
+                                               // MessageBox.Show("Parsuju ID");
+                                                id += znak1;
+                                            }
+                                            if (parsingInformation == 2) // Parsuju CSV parametry
+                                            {                                             
+                                                csvParametrs += znak1;
+                                            }
+                                        }
+                                    }
+
+                                }
+
+                                if (csvParametrs != "")
+                                {
+                                    MessageBox.Show(csvParametrs);
+                                }
+
+                                save.Write("[oGold]" + "[" + csvParametrs + "]");
+                            }
+                            save.WriteLine();
+                            x++;
+                        }
+                        save.WriteLine("#NewLine");
+                    }
+
+                    y++;
+                    x = 0;
+          
+                
             }
 
             save.Close();
@@ -226,6 +313,242 @@ namespace RetroRpgLevelEditor
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            oIDEditor.Text = shtb.Text;
+            oIDEditor.ZoomFactor = 0.02F;
+        }
+
+        private void oCompile_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            int minID = 1;
+            int point = shtb.SelectionStart;
+           // MessageBox.Show(Convert.ToString(point));       
+            int dataLine = 0;
+            foreach (string line in shtb.Lines)
+            {
+                dataLine++;
+
+                if (line == "@DataPipeline")
+                {
+                    break;
+                }
+            }
+
+
+            foreach (string line in shtb.Lines)
+            {               
+
+                if (line.StartsWith("//"))
+                {
+ 
+                }
+                else if (line.StartsWith("@DataPipeline"))
+                {
+
+                }
+                else
+                {
+                    string id = "";
+
+                    foreach (char znak in line)
+                    {
+                        if (znak == '#')
+                        {
+                           
+                        }
+                        else if (znak == ' ')
+                        {
+                           
+                        }
+                        else if (znak == '¤')
+                        {
+                            bool parsing = false;
+                            int xx = 0;                            
+
+                            foreach (char znak1 in line)
+                            {
+                                xx++;
+
+                                if (znak1 == '[' && parsing == false && xx > x )
+                                {
+                                    parsing = true;
+                                }
+                                else if (parsing)
+                                {
+                                    if (znak1 != ']' && znak1 != ' ')
+                                    {
+                                        id += znak1;
+                                    }
+                                    if (znak1 == ']')
+                                    {
+                                        if (Convert.ToInt32(id) >= minID)
+                                        {
+                                            minID = Convert.ToInt32(id) + 1;
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                            /*
+                            for (int i = dataLine; i < shtb.Lines.Count(); i++ )
+                            {
+                                int parsingInformation = 0;
+                                string xx = "";
+                                string yy = "";
+                                string value = "";
+
+                                foreach(char )
+
+                            }*/
+                         
+                        }                       
+                        x++;
+                    }
+                }
+
+                y++;
+                x = 0;
+            }
+            //MessageBox.Show("Use ID " + Convert.ToString(minID));
+            shtb.Text = shtb.Text.Insert(point, " [ " + Convert.ToString(minID) + " ]");//AppendText(" [ " + Convert.ToString(minID) + " ]");
+        }
+
+        private void oZoomPlus_Click(object sender, EventArgs e)
+        {
+            shtb.ZoomFactor += 0.5F;
+        }
+
+        private void oZoomMinus_Click(object sender, EventArgs e)
+        {
+            shtb.ZoomFactor -= 0.5F;
+        }
+
+        private void oZoomCenter_Click(object sender, EventArgs e)
+        {
+            shtb.ZoomFactor = 1;
+        }
+
+     
+        void shtbDoubleClick(object Sender, EventArgs e)
+        {
+            oCompile_Click(Sender, e);
+        }
+
+        private void CompileCode_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            int minID = 1;
+            int point = shtb.SelectionStart;
+            // MessageBox.Show(Convert.ToString(point));       
+            int dataLine = 0;
+            foreach (string line in shtb.Lines)
+            {
+                dataLine++;
+
+                if (line == "@DataPipeline")
+                {
+                    break;
+                }
+            }
+
+            if (!shtb.Text.Contains("@DataPipeline"))
+            {
+                shtb.AppendText("\n@DataPipeline\n");
+            }
+
+            foreach (string line in shtb.Lines)
+            {
+
+                if (line.StartsWith("//"))
+                {
+
+                }
+                else if (line.StartsWith("@DataPipeline"))
+                {
+
+                }
+                else
+                {
+                    string id = "";
+
+                    foreach (char znak in line)
+                    {
+                        if (znak == '#')
+                        {
+
+                        }
+                        else if (znak == ' ')
+                        {
+
+                        }
+                        else if (znak == '¤')
+                        {
+                            bool parsing = false;
+                            int xx = 0;
+
+                            foreach (char znak1 in line)
+                            {
+                                xx++;
+
+                                if (znak1 == '[' && parsing == false && xx > x)
+                                {
+                                    parsing = true;
+                                }
+                                else if (parsing)
+                                {
+                                    if (znak1 != ']' && znak1 != ' ')
+                                    {
+                                        id += znak1;
+                                    }
+                                    if (znak1 == ']')
+                                    {
+                                        int returnToY = y;
+                                       // ScrollToLine(dataLine);
+                                        shtb.AppendText("[ " + id + " ]" + "[ oCoin ]" + "\n");
+                                     //   ScrollToLine(returnToY);
+                                        break;
+                                    }
+                                }
+                            }
+                            /*
+                            for (int i = dataLine; i < shtb.Lines.Count(); i++ )
+                            {
+                                int parsingInformation = 0;
+                                string xx = "";
+                                string yy = "";
+                                string value = "";
+
+                                foreach(char )
+
+                            }*/
+
+                        }
+                        x++;
+                    }
+                }
+
+                y++;
+                x = 0;
+            }
+            //MessageBox.Show("Use ID " + Convert.ToString(minID));
+            //shtb.Text = shtb.Text.Insert(point, " [ " + Convert.ToString(minID) + " ]");//AppendText(" [ " + Convert.ToString(minID) + " ]");
+        }
+
+        void ScrollToLine(int lineNumber)
+        {
+            if (lineNumber > shtb.Lines.Count()) return;
+
+            try
+            {
+                shtb.SelectionStart = shtb.Find(shtb.Lines[lineNumber]);
+                shtb.ScrollToCaret();
+            }
+            catch
+            {
+
+            }
         }
     }
 }
