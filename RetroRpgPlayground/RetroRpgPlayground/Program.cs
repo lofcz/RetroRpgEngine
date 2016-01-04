e6b328aae376fca8ef06ab4f043ddb7ac9fd6fac
kocourmatej <kocourmatej@centrum.cz>
2016-01-04 17:57:22 +0100

    Added playground

---
+++ RetroRpgPlayground/RetroRpgPlayground/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroRpgPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 400; i++)
+            {
+                // Get ascii character.
+                char c = (char)i;
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
